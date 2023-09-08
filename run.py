f014a0ab322903358344ae7d280c31c86479bc57
Renwar-P <>
2023-09-08 18:32:16 +0000

    add more calls to main and repositioning quote_list

--- run.py
+++ run.py
@@ -1,26 +1,40 @@
 import random
 
 
+quote_list = [
+    'Life does not end when we die, it ends when we give up',
+    'All our dreams can come true, if we have the courage to pursue them',
+    'Happiness is not something ready made. It comes from your own actions',
+    'The secret of getting ahead is getting started',
+    'Whatever you are, be a good one',
+    'Impossible is just an opinion',
+    'Your passion is waiting for your courage to catch up',
+    'Hold the vision, trust the process',
+    'Do not be afraid to give up the good to go for the great',
+    'Keep your eyes on the stars, and your feet on the ground'
+]
+
 def welcome_quote_of_the_day():
     """
     Function to greet and start the game
     """
     print('Welcome to Quote of the Day!\n')
-    print('Please enter the name of the current day')
 
 
 def check_input(user_input):
     """
     Function to check if input is valid
     """
-    inputs_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
-    'Saturday', 'Sunday']
+    inputs_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     if user_input in inputs_list:
-        print(random_quote) 
+        random_quote = display_random_quote()
+        print(f"\nHere's a random quote for {user_input}:")
+        print(random_quote)
+    
     else:
         print('Please enter a valid weekday')
-
+        
 
 def display_random_quote():
     """
@@ -30,28 +44,22 @@ def display_random_quote():
     return random_quote
 
 
-a = 'Life does not end when we die, it ends when we give up'
-b = 'All our dreams can come true, if we have the courage to pursue them'
-c = 'Happiness is not something ready made. It comes from your own actions'
-d = 'The secret of getting ahead is getting started'
-e = 'Whatever you are, be a good one'
-f = 'Impossible is just an opinion'
-g = 'Your passion is waiting for your courage to catch up'
-h = 'Hold the vision, trust the process'
-i = 'Do not be afraid to give up the good to go for the great'
-j = 'Keep your eyes on the stars, and your feet on the ground'
-
-quote_list = [a, b, c, d, e, f, g, h, i, j]
-
-
 def game_end():
     """
     Function to run when the game ends
     """
+    print('Thank you for playing!')
 
 
 def main():
     """
-    Function to run all 
+    Function to run the entire program
     """
     welcome_quote_of_the_day()
+    day_name = input("Enter the name of today: ")
+    check_input(day_name)
+    game_end()
+
+
+if __name__ == "__main__":
+    main()
