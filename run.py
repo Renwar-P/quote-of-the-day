4bccd24297862a6a261a99c86e6d8c3c5ad12d51
Renwar-P <>
2023-09-08 11:00:41 +0000

    initial commit with main structure of functions

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+def welcome_quote_of_the_day():
+    """
+    Function to greet and start the game
+    """
+    
+
+def check_input():
+    """
+    Function to check if input is valid
+    """
+   
+
+def display_random_quote():
+    """
+    Function to get the random quote
+    """
+   
+
+def game_end():
+    """
+    Function to run when the game ends
+    """
+    
