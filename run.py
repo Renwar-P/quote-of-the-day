c9aafb0f8a3992a67840869bad35085fa4ae7eee
Renwar-P <>
2023-09-08 13:05:15 +0000

    add main function and quote_list

--- run.py
+++ run.py
@@ -1,29 +1,48 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-
+import random
 
 
 def welcome_quote_of_the_day():
     """
     Function to greet and start the game
     """
-    
+    print('Welcome to Quote of the Day!\n')
+    print('Please enter the name of the current day')
+
 
 def check_input():
     """
     Function to check if input is valid
     """
-   
+
 
 def display_random_quote():
     """
-    Function to get the random quote
+    Function to get a random quote
     """
-   
+  
+
+a = 'Life does not end when we die, it ends when we give up'
+b = 'All our dreams can come true, if we have the courage to pursue them'
+c = 'Happiness is not something ready made. It comes from your own actions'
+d = 'The secret of getting ahead is getting started'
+e = 'Whatever you are, be a good one'
+f = 'Impossible is just an opinion'
+g = 'Your passion is waiting for your courage to catch up'
+h = 'Hold the vision, trust the process'
+i = 'Do not be afraid to give up the good to go for the great'
+j = 'Keep your eyes on the stars, and your feet on the ground'
+
+quote_list = [a, b, c, d, e, f, g, h, i, j]
+
 
 def game_end():
     """
     Function to run when the game ends
     """
-    
+
+
+def main():
+    """
+    Function to run all 
+    """
+    welcome_quote_of_the_day()
